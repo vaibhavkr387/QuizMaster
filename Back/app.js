88b378296a80d7a29e95663b811fdf6b386c9e48
vaibhavkr387 <vaibhavkr387@gmail.com>
2025-04-17 21:16:32 +0530

    Update app.js

--- Back/app.js
+++ Back/app.js
@@ -20,12 +20,31 @@ dotenv.config();
 const app = express();
 
 // Middleware
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://quiz-master-opal.vercel.app',
+];
+
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload({
